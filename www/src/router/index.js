cd82fda817528e7a73dd78268bc68d442db563f8
Caleb Adrian <calebadrian1847@gmail.com>
2018-02-22 12:04:18 -0700

    re init vue

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Slap from '@/components/Slap'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Slap',
-      component: Slap
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
